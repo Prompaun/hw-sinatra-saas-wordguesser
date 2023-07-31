57f4f982be3d36abd04acda168f34c87af70af17
pmyyyy <prompaun@gmail.com>
2023-07-31 07:53:37 +0000

    hw-sinatra-saas-wordguesser ver.2

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,45 +1,6 @@
 class WordGuesserGame
 
   attr_accessor :word, :guesses, :wrong_guesses
-<<<<<<< HEAD
-
-  def initialize(word)
-    @word = word
-    @guesses = ''
-    @wrong_guesses = ''
-  end
-
-  def guess(letter)
-    if letter.nil? || letter !~ /\A[a-zA-Z]\Z/i # match single letter,ignoring case
-      raise ArgumentError
-    end
-    letter = letter[0].downcase
-    if  @word.include?(letter) && !@guesses.include?(letter)
-      @guesses += letter
-    elsif !@word.include?(letter) && !@wrong_guesses.include?(letter)
-      @wrong_guesses += letter
-    else
-      false
-    end
-  end
-
-  def word_with_guesses
-    @word.gsub(/./)  { |letter| @guesses.include?(letter) ? letter : '-' }
-  end
-
-  def check_win_or_lose
-    if word_with_guesses == @word
-      :win
-    elsif @wrong_guesses.length > 6
-      :lose
-    else
-      :play
-    end
-  end
-
-  # Get a word from remote "random word" service
-
-=======
 
   def initialize(word)
     puts word
@@ -78,7 +39,6 @@ class WordGuesserGame
 
   # Get a word from remote "random word" service
 
->>>>>>> 8abc250 (hw-sinatra-saas-wordguesser ver.1)
   def self.get_random_word
     require 'uri'
     require 'net/http'
